1ec9a059802e96e69c90acc84803ed231b6dca82
Артём Аборнев <21se90210@gmail.com>
2021-04-13 13:49:12 +0700

    app module mysql db

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 import os
 
-app = Flask(__name__, template_folder=os.getcwd() + 'app\\templates',
-            static_url_path='', static_folder=os.path.join(os.getcwd(), 'app\\static'))
+
+app = Flask(__name__, static_url_path='', static_folder=os.path.join(os.getcwd(), 'app\\static'))
 app.debug = True
 app.config['SECRET_KEY'] = 'Q(@fj29j73fas@j7sda*J@'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:av4*A@VM78n@CM#*97@localhost/todolist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'app\\static\\uploads'
 
